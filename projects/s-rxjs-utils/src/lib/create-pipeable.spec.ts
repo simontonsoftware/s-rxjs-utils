c32bebd37de05120ba8325be5d08651fa09ecea8
Eric <eric@simontonsoftware.com>
2018-09-01 11:15:54 -0400

    chore: document `createPipeable()`

--- projects/s-rxjs-utils/src/lib/create-pipeable.spec.ts
+++ projects/s-rxjs-utils/src/lib/create-pipeable.spec.ts
@@ -1,5 +1,6 @@
-import { createPipeable } from './create-pipeable';
 import { Observable, of, Subscriber } from 'rxjs';
+import { toArray } from 'rxjs/operators';
+import { createPipeable } from './create-pipeable';
 import { SubscriptionManager } from './subscription-manager';
 
 describe('createPipeable()', () => {
@@ -24,5 +25,43 @@ describe('createPipeable()', () => {
 
     // the real test is just that the above does not give a type error
     expect().nothing();
+  });
+
+  it('works for the example in the documentation', async () => {
+    function map<I, O>(fn: (input: I) => O) {
+      return createPipeable(
+        (
+          upstream: Observable<I>,
+          downstream: Subscriber<O>,
+          subscriptionManager: SubscriptionManager
+        ) => {
+          subscriptionManager.subscribeTo(
+            upstream,
+            value => {
+              downstream.next(fn(value));
+            },
+            downstream.error.bind(downstream),
+            downstream.complete.bind(downstream)
+          );
+        }
+      );
+    }
+
+    expect(
+      await of(1, 2, 3)
+        .pipe(
+          map(i => i + 1),
+          toArray()
+        )
+        .toPromise()
+    ).toEqual([2, 3, 4]);
+    expect(
+      await of(1, 2, 3)
+        .pipe(
+          map(i => i.toString()),
+          toArray()
+        )
+        .toPromise()
+    ).toEqual(['1', '2', '3']);
   });
 });
--- projects/s-rxjs-utils/src/lib/create-pipeable.ts
+++ projects/s-rxjs-utils/src/lib/create-pipeable.ts
@@ -1,13 +1,38 @@
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, OperatorFunction, Subscriber } from 'rxjs';
 import { SubscriptionManager } from './subscription-manager';
 
+/**
+ * Use this to create a complex pipeable operator. It is usually better style to compose existing operators than to create a brand new one, but when you need full control this can reduce some boilerplate.
+ *
+ * A simple example, recreating the "map" operator:
+ * ```ts
+ * function map<I, O>(fn: (input: I) => O) {
+ *   return createPipeable(
+ *     (
+ *       upstream: Observable<I>,
+ *       downstream: Subscriber<O>,
+ *       subscriptionManager: SubscriptionManager
+ *     ) => {
+ *       subscriptionManager.subscribeTo(
+ *         upstream,
+ *         value => {
+ *           downstream.next(fn(value));
+ *         },
+ *         downstream.error.bind(downstream),
+ *         downstream.complete.bind(downstream)
+ *       );
+ *     }
+ *   );
+ * }
+ * ```
+ */
 export function createPipeable<UpstreamType, DownstreamType = UpstreamType>(
   subscribe: (
     upstream: Observable<UpstreamType>,
     downstream: Subscriber<DownstreamType>,
     subscriptionManager: SubscriptionManager
   ) => void
-) {
+): OperatorFunction<UpstreamType, DownstreamType> {
   return (source: Observable<UpstreamType>) =>
     new Observable<DownstreamType>(subscriber => {
       const manager = new SubscriptionManager();
