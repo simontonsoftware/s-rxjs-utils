5abf8a3b97c1106bf7b8fcbff6521c8c1512c75e
Eric <eric@simontonsoftware.com>
2018-08-31 20:43:13 -0400

    chore: remove scaffolded library code

--- projects/s-rxjs-utils/src/lib/s-rxjs-utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SRxjsUtilsComponent } from './s-rxjs-utils.component';
-
-@NgModule({
-  imports: [],
-  declarations: [SRxjsUtilsComponent],
-  exports: [SRxjsUtilsComponent]
-})
-export class SRxjsUtilsModule {}
--- projects/s-rxjs-utils/src/public_api.ts
+++ projects/s-rxjs-utils/src/public_api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of s-rxjs-utils
  */
-
-export * from './lib/s-rxjs-utils.service';
-export * from './lib/s-rxjs-utils.component';
-export * from './lib/s-rxjs-utils.module';
