5f0c28fe90e577f464af46af648c4cf95ca232e8
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-15 19:03:49 -0400

    chore: improve docs for `logValues()``

--- projects/s-rxjs-utils/src/lib/operators/log-values.ts
+++ projects/s-rxjs-utils/src/lib/operators/log-values.ts
@@ -2,6 +2,24 @@ import { tap } from "rxjs/operators";
 
 /**
  * Logs values, errors and completion to the console, and passes them all along unchanged.
+ *
+ * ```ts
+ * of(1, 2).pipe(logValues()).subscribe();
+ * // prints using console.log:
+ * // [value] 1
+ * // [value] 2
+ * // [complete]
+ *
+ * of(1, 2).pipe(logValues("my number", "debug")).subscribe();
+ * // prints using console.debug:
+ * // [value] my number 1
+ * // [value] my number 2
+ * // [complete] my number
+ *
+ * throwError("boo").pipe(logValues("pipe says", "warn")).subscribe();
+ * // prints using console.warn:
+ * // [error] pipe says boo
+ * ```
  */
 export function logValues(
   prefix?: string,
