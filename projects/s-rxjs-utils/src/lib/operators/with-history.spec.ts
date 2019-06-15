40ea199d79dfaf83c2d2ba2447ccf7aee16a55c1
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-14 21:53:52 -0400

    chore: fun with `marbleTest`

--- projects/s-rxjs-utils/src/lib/operators/with-history.spec.ts
+++ projects/s-rxjs-utils/src/lib/operators/with-history.spec.ts
@@ -1,27 +1,40 @@
 import {
-  expectPipeResult,
   testCompletionPropagation,
   testErrorPropagation,
   testUnsubscribePropagation,
 } from "../../test-helpers/misc-helpers";
+import { marbleTest } from "../../to-replace/marble-test";
 import { withHistory } from "./with-history";
 
 describe("withHistory()", () => {
-  it("emits the last `historyCount` values", async () => {
-    await expectPipeResult([1, 2, 3, 4], withHistory(2), [
-      [1],
-      [2, 1],
-      [3, 2, 1],
-      [4, 3, 2],
-    ]);
-    await expectPipeResult([1, 2, 3, 4], withHistory(1), [
-      [1],
-      [2, 1],
-      [3, 2],
-      [4, 3],
-    ]);
-    await expectPipeResult([1, 2, 3, 4], withHistory(0), [[1], [2], [3], [4]]);
-  });
+  it(
+    "emits the last `historyCount` values",
+    marbleTest(({ cold, expectObservable, expectSubscriptions }) => {
+      const source = cold("-1-2-3-4-|");
+      const subs = "       ^--------!";
+      const expected = "   -a-b-c-d-|";
+
+      expectObservable(source.pipe(withHistory(2))).toBe(expected, {
+        a: ["1"],
+        b: ["2", "1"],
+        c: ["3", "2", "1"],
+        d: ["4", "3", "2"],
+      });
+      expectObservable(source.pipe(withHistory(1))).toBe(expected, {
+        a: ["1"],
+        b: ["2", "1"],
+        c: ["3", "2"],
+        d: ["4", "3"],
+      });
+      expectObservable(source.pipe(withHistory(0))).toBe(expected, {
+        a: ["1"],
+        b: ["2"],
+        c: ["3"],
+        d: ["4"],
+      });
+      expectSubscriptions(source.subscriptions).toBe([subs, subs, subs]);
+    }),
+  );
 
   it("passes along unsubscribes", () => {
     testUnsubscribePropagation(() => withHistory(1));
